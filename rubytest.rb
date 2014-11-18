dad35a282640dfd1221d8ed88a3685efa1a5ada8
Corey Harada <haradacm@gmail.com>
2014-11-17 18:23:24 -0800

    Step 5

--- rubytest.rb
+++ rubytest.rb
@@ -5,15 +5,24 @@ def Add(numbers)
   else
     delim = /[\n|,]/;
   end
+  negatives = [];
   parsedNums = numbers.split(delim);
   sum = 0;
   parsedNums.each do |num|
+    if num.to_i < 0
+      negatives.push(num)
+    end
     sum += num.to_i
   end
-  sum
+  if negatives.empty?
+    sum
+  else
+    raise "negatives not allowed " + negatives.join(",")
+  end
 end
 
 puts Add("1,2,3,4,5,6")
 puts Add("1\n2,3")
 puts Add("1\n,2")
 puts Add("//g\n1g5g4")
+puts Add("-1,2,-3")
