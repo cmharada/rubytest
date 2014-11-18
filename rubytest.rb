345e5b48fe60d6a691fb7540b986bf600c95eeca
Corey Harada <haradacm@gmail.com>
2014-11-17 18:02:46 -0800

    Step 3

--- rubytest.rb
+++ rubytest.rb
@@ -1,5 +1,5 @@
 def Add(numbers)
-  parsedNums = numbers.split(",");
+  parsedNums = numbers.split(/[\n|,]/);
   sum = 0;
   parsedNums.each do |num|
     sum += num.to_i
@@ -8,3 +8,5 @@ def Add(numbers)
 end
 
 puts Add("1,2,3,4,5,6")
+puts Add("1\n2,3")
+puts Add("1\n,2")
