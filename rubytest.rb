d4ea077654b52195f1889a0576b6e318aef8449b
Corey Harada <haradacm@gmail.com>
2014-11-17 18:20:11 -0800

    Step 4

--- rubytest.rb
+++ rubytest.rb
@@ -1,5 +1,11 @@
 def Add(numbers)
-  parsedNums = numbers.split(/[\n|,]/);
+  if numbers.start_with?("//")
+    delim = numbers.slice(2...numbers.index("\n"))
+    puts delim
+  else
+    delim = /[\n|,]/;
+  end
+  parsedNums = numbers.split(delim);
   sum = 0;
   parsedNums.each do |num|
     sum += num.to_i
@@ -10,3 +16,4 @@ end
 puts Add("1,2,3,4,5,6")
 puts Add("1\n2,3")
 puts Add("1\n,2")
+puts Add("//g\n1g5g4")
